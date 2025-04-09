e79262bec36f8420e94f634b0dc6812cca9ec8c0
Tobi Adetula <oluwatobi.adetula1@gmail.com>
2025-04-09 10:22:32 +0200

    removed checkout to DIII branch

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -89,14 +89,15 @@ setup_git()
 	git apply --reject $PATCHES_DIR/hdl_repo24.patch
 	git add -A && git commit -m "MPSoC4Drones"
 	git tag -f $DIII_REPO_TAG HEAD
-	git checkout $AVNET_REPO_TAG && git checkout $DIII_REPO_TAG
+	git checkout $AVNET_REPO_TAG 
 
 	cd $REPOSITORY_DIR/petalinux
 	git checkout $AVNET_REPO_TAG
 	git apply --reject $PATCHES_DIR/petalinux_repo24.patch
+
 	git add -A && git commit -m "MPSoC4Drones"
 	git tag -f $DIII_REPO_TAG HEAD
-	git checkout $AVNET_REPO_TAG && git checkout $DIII_REPO_TAG
+	git checkout $AVNET_REPO_TAG 
 
 	cd $REPOSITORY_DIR/meta-avnet
 	git checkout $AVNET_REPO_TAG
